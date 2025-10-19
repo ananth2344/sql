d74d7e58d576e423f4e31f3595758de6142b3e60
ananth2344 <krishnananantha093@gmail.com>
2025-10-19 21:10:08 +0530

    commit1

--- hw1wrk.sql
+++ hw1wrk.sql
@@ -4,11 +4,7 @@ INSERT INTO books (id, title, author, price, genre) VALUES
 (3, 'The Da Vinci Code', 'Dan Brown', 450, 'Mystery'),
 (4, 'Sapiens: A Brief History of Humankind', 'Yuval Noah Harari', 600, 'History'),
 (5, 'To Kill a Mockingbird', 'Harper Lee', 380, 'Fiction');
-SELECT * FROM books
-WHERE price > 400;
-SELECT * FROM books
-WHERE genre IN ('History', 'Science', 'Fiction');
-SELECT * FROM books
-WHERE title = 'The Great Gatsby';
-SELECT * FROM books
-WHERE author <> 'Dan Brown';
+SELECT * FROM books WHERE price > 400;
+SELECT * FROM books WHERE genre IN ('History', 'Science', 'Fiction');
+SELECT * FROM books WHERE title = 'The Great Gatsby';
+SELECT * FROM books WHERE author <> 'Dan Brown';
