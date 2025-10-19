506d8b8fed1bcfe20a9b8f554cdde70aa55a3c11
ananth2344 <krishnananantha093@gmail.com>
2025-10-19 21:49:09 +0530

    commit

--- hw6wrk.sql
+++ hw6wrk.sql
@@ -1,22 +1,19 @@
-CREATE TABLE students (
+CREATE TABLE users (
     id INT PRIMARY KEY,
     name TEXT,
-    course TEXT,
-    score INT,
-    email TEXT,
-    phone TEXT,
     city TEXT,
-    bonus_points INT
+    score INT,
+    bonus INT,
+    challenge TEXT
 );
-INSERT INTO students (id, name, course, score, email, phone, city, bonus_points) VALUES
-(1, 'Asha', 'Python', 85, 'asha@mail.com', '9876543210', 'Chennai', 5),
-(2, 'Ravi', 'Python', 90, 'ravi@mail.com', '9876543211', 'Chennai', NULL),
-(3, 'Sneha', 'Java', 78, 'sneha@mail.com', '9876543212', 'Mumbai', NULL),
-(4, 'Karan', 'Java', 88, 'karan@mail.com', '9876543213', 'Delhi', 2),
-(5, 'Divya', 'Python', 95, 'divya@mail.com', '9876543214', 'Mumbai', 4),
-(6, 'Manoj', 'JavaScript', 72, 'manoj@mail.com', '9876543215', 'Delhi', NULL);
-SELECT course, COUNT(*) AS total_students FROM students GROUP BY course;
-SELECT course, AVG(score) AS average_score FROM students GROUP BY course HAVING AVG(score) > 80;
-SELECT name, score FROM students WHERE city IN ('Chennai', 'Mumbai');
-SELECT * FROM students WHERE bonus_points IS NULL;
-SELECT name FROM students WHERE city = 'Chennai' UNION SELECT name FROM students WHERE city = 'Mumbai';
+INSERT INTO users (id, name, city, score, bonus, challenge) VALUES
+(1, 'Raj', 'Chennai', 88, 5, 'Fitness'),
+(2, 'Anu', 'Mumbai', 91, NULL, 'Diet'),
+(3, 'Ravi', 'Chennai', 78, 3, 'Fitness'),
+(4, 'Meena', 'Delhi', 82, NULL, 'Diet'),
+(5, 'Farah', 'Mumbai', 95, 4, 'Fitness'),
+(6, 'Kiran', 'Pune', 70, NULL, 'Yoga'),
+(7, 'Latha', 'Pune', 87, NULL, 'Fitness');
+SELECT * FROM users WHERE score > (SELECT AVG(score) FROM users);
+SELECT name FROM users WHERE challenge = (SELECT challenge  FROM users  WHERE name = 'Farah'
+);
